7b57c0b36d95f65bd279899389d32edfa2ea8f59
micro <damianohart120905@gmail.com>
2022-02-13 13:30:29 -0500

    axios + ready event

--- src/config-example.js
+++ src/config-example.js
@@ -1,3 +1,4 @@
 module.exports = {
-    BOT_TOKEN: "" // The bot token.
+    BOT_TOKEN: "", // The bot token.
+    DANBOT_API_KEY: "" // The api key for the DanBot api.
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+const { default: axios } = require("axios");
 const { Client } = require("discord.js");
 
 const client = new Client({ intents: 503 });
 
 client.config = require("./config");
+
+client.on("ready", () => {
+    axios({
+        method: "POST",
+        url: "https://danbot.host/api/bot/" + client.user.id + "/stats",
+        data: {
+            apiKey: client.config.DANBOT_API_KEY,
+            users: 1,
+            servers: 1,
+            clientInfo: {
+                message: "Get trolled."
+            }
+        }
+    });
+});
 
 client.login(client.config.BOT_TOKEN);
